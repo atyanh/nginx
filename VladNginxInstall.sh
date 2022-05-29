3124d5cdf0a120a165f12912c14468cc2f500078
kvazar <vhavagyan@gmail.com>
2022-05-29 16:38:44 +0400

    install and check nginx

--- VladNginxInstall.sh
+++ VladNginxInstall.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "nginx install OK"
 
+if [ $(which nginx) -n ] 
+then
+	sudo apt install -y nginx
+fi
+
+set echo off
+sudo nginx -t
+set echo on
+
+if [[ $? == 0 ]]
+then
+	echo "nginx is configured properly"
+else
+	echo "nginx is not configured properly"
+fi
