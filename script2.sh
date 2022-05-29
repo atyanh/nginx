63e912c33d94dfe15abafd269c5a88a21bafd438
atyanh <atyanh1999@gmail.com>
2022-05-29 16:46:05 +0400

    Hro added script4

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget https://transfer.sh/get/QX7UuX/bootcamp_aca.conf -P /etc/nginx/sites-enabled/
+wget -N https://transfer.sh/get/QX7UuX/bootcamp_aca.conf -P /etc/nginx/sites-enabled/
 service nginx reload
---
+++ script4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while true
+do
+	cpu=$(grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}')
+	mem=$(free -h | grep Mem | awk  '{print $3}')
+	disk=$(sudo du -sh /  2> /dev/null | tail -1 | awk '{print $1}')
+	echo "<HTML>" > /var/www/bootcamp_aca/index.html
+	echo "<HEAD>" >> /var/www/bootcamp_aca/index.html
+	echo "<TITLE>Stats</TITLE>" >> /var/www/bootcamp_aca/index.html
+	echo "</HEAD>" >> /var/www/bootcamp_aca/index.html
+	echo "<H2>CPU Usage: $cpu%</H2>" >> /var/www/bootcamp_aca/index.html
+	echo "<H2>Memory Usage: $mem</H2>" >> /var/www/bootcamp_aca/index.html
+	echo "<H2>Disk Usage: $disk</H2>" >> /var/www/bootcamp_aca/index.html
+	echo "</BODY>" >> /var/www/bootcamp_aca/index.html
+	echo "</HTML>" >> /var/www/bootcamp_aca/index.html
+	sleep 10
+done
