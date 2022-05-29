89a6b06fc14a64993c12b1c3eadf87f7e36c5aea
kvazar <vhavagyan@gmail.com>
2022-05-30 00:20:09 +0400

    create index file

--- VladNginxCreateIndex.sh
+++ VladNginxCreateIndex.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-echo "nginx index create OK"
+
+conffilename="/etc/nginx/sites-enabled/bootcamp_aca.conf"
+
+siterootdir=$(sed -n '/root /p' $conffilename | awk '{ print $2 }' | sed 's/;//')
+indexfilename=$(sed -n '/index /p' $conffilename | awk '{ print $2 }' | sed 's/;//')
+
+sudo mkdir -p $siterootdir
+
+htmltext="<html>
+	<head>
+		<title>Simple resource monitor</title>
+	</head>
+	<body>
+	<p>$(date)</p>
+	</body>
+</html>"
+
+echo $htmltext | sudo tee $siterootdir/$indexfilename >/dev/null
+
+
+curl -s localhost >/dev/null
+if [ $? == 0 ]
+then
+	echo "Index html works"
+else
+	echo "Index html does not work"
+fi
 
