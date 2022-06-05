0d05f940e704189baac81d33fe5d41dd3109c569
atyanh <93996923+atyanh@users.noreply.github.com>
2022-06-05 16:33:55 +0400

    Update update_stats.sh

--- Hrayr_Version/update_stats.sh
+++ Hrayr_Version/update_stats.sh
@@ -3,19 +3,19 @@
 #Checking Statisctics and sending it to web page
 #They will be checked every 10 seconds
 
-while true
-do
+#while true
+#do
 	cpu=$(grep 'cpu ' /proc/stat | awk '{print ($2+$4)*100/($2+$4+$5)}')
 	mem=$(free -h | grep Mem | awk  '{print $3}')
 	disk=$(sudo du -sh /  2> /dev/null | tail -1 | awk '{print $1}')
-	echo "<HTML>" > /var/www/bootcamp_aca/index.html
-	echo "<HEAD>" >> /var/www/bootcamp_aca/index.html
-	echo "<TITLE>Stats</TITLE>" >> /var/www/bootcamp_aca/index.html
-	echo "</HEAD>" >> /var/www/bootcamp_aca/index.html
-	echo "<H2>CPU Usage: $cpu%</H2>" >> /var/www/bootcamp_aca/index.html
-	echo "<H2>Memory Usage: $mem</H2>" >> /var/www/bootcamp_aca/index.html
-	echo "<H2>Disk Usage: $disk</H2>" >> /var/www/bootcamp_aca/index.html
-	echo "</BODY>" >> /var/www/bootcamp_aca/index.html
-	echo "</HTML>" >> /var/www/bootcamp_aca/index.html
-	sleep 10
-done
+#	echo "<HTML>" > /var/www/bootcamp_aca/index.html
+#	echo "<HEAD>" >> /var/www/bootcamp_aca/index.html
+#	echo "<TITLE>Stats</TITLE>" >> /var/www/bootcamp_aca/index.html
+#	echo "</HEAD>" >> /var/www/bootcamp_aca/index.html
+	echo "<H2>CPU Usage: $cpu%</H2>" #>> /var/www/bootcamp_aca/index.html
+	echo "<H2>Memory Usage: $mem</H2>" #>> /var/www/bootcamp_aca/index.html
+	echo "<H2>Disk Usage: $disk</H2>" #>> /var/www/bootcamp_aca/index.html
+#	echo "</BODY>" >> /var/www/bootcamp_aca/index.html
+#	echo "</HTML>" >> /var/www/bootcamp_aca/index.html
+#	sleep 10
+#done
